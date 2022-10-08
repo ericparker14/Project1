fe879874357c8e87398ee90beb4384d3ea6feac4
AC3-OF-SP4D3S <105685613+AC3-OF-SP4D3S@users.noreply.github.com>
2022-10-08 05:39:19 -0500

    Triangle animation
    Added switch case scenario for Triangle animation
    Edited the "Would you like to continue?" questions and the Quit case to loop until the user gives a valid answer

--- ac1414Project1.cpp
+++ ac1414Project1.cpp
@@ -19,15 +19,18 @@ int main() {
     bool continueOn = true;
     char next;
     
+    system("clear");
     cout << "Please enter an odd number: "; 
     cin >> length;
     
     while ((length % 2) == 0){
-        cout << "Number is not Odd.\nPlease enter an odd number: ";
+        system("clear");
+        cout << "Number is not odd.\nPlease enter an odd number: ";
         cin >> length;
 	}
+    system("clear");
     do{
-        cout << "What shape would you like to create with a length of "<<length<<"?\n(1) Square\n(2) Triangle\n(3) Pentagon\n(4) Sentence\n(5) Quit\nChoose an option: ";
+        cout << "What shape would you like to create with a length of "<<length<<"?\n(1) Square\n(2) Triangle\n(3) Pentagon\n(4) Sentence\n(5) Quit\n\nChoose an option: ";
         cin >> userChoice;
         choice = static_cast<Shape>(userChoice);
         
@@ -48,35 +51,65 @@ int main() {
                     }
                     sleep(1);
                 }
-                cout << "Would you like to continue? (y/n): ";
-                cin >> next;
-                next = tolower(next);
-                if(next	== 'n'){
-                    continueOn = false;
-                }
-                else{
-                    continue;
-                }
+                do{
+                    cout << "Would you like to continue? (Y/N): ";
+                    cin >> next;
+                    next = toupper(next);
+                    if(next == 'Y'){
+                        continueOn = true;
+                    }
+                    else if(next == 'N'){
+                        continueOn = false;
+                    }
+                }while(next!='Y'&&next!='N');
                 break;
             case Triangle: /* Generate a Triangle */
+                system("clear");
+                for(int numLine = 1; numLine <= length; numLine++){
+                    for(int numRow = 0; numRow < length-numLine; numRow++){ /* rows */
+                        cout << " ";
+                    }
+                    for(int numCol = 0; numCol < ((2*numLine)-1); numCol++){ /* columns */
+                        cout << signChar;
+                    }
+                    cout << endl;
+                    sleep(1);
+                }
+                do{
+                    cout << "Would you like to continue? (Y/N): ";
+                    cin >> next;
+                    next = toupper(next);
+                    if(next == 'Y'){
+                        continueOn = true;
+                    }
+                    else if(next == 'N'){
+                        continueOn = false;
+                    }
+                }while(next!='Y'&&next!='N');
                 break;
             case Pentagon: /* Generate a Pentagon */
                 break;
             case Sentence: /* Sentence for animation */
                 break;
-            case Quit:
-                cout << "Would you like to quit the program? (y/n): ";
-                cin >> next;
-                if(next	== 'n'){ /* Ask user to continue program. If not, end program */
-                    continueOn = false;
-                }
-		else{
-                    continue;
-                }
+            case Quit: /* Ask user to continue program. If not, end program */
+                do{
+                    system("clear");
+                    cout << "Are you sure you would like to quit the program? (Y/N): ";
+                    cin >> next;
+                    next = toupper(next);
+                    if(next == 'Y'){
+                        continueOn = false;
+                    }
+                    else if(next == 'N'){
+                        continueOn = true;
+                        system("clear");
+                    }
+                }while(next!='Y'&&next!='N');
                 break;
             default: /* Error message */
-                cout << "Invalid input. Please enter a valid input.\n";
-                break;}
+                cout << "\nInvalid input. Please enter a valid input.\n\n";
+                break;
+        }
     }while(continueOn == true);
     
     cout << "Goodbye!\n";
