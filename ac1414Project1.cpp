b8604f96672ba3e665814f685eba2a314baf97ca
ericparker14 <ericparker243@gmail.com>
2022-10-08 21:49:38 -0500

    Sentence case

--- ac1414Project1.cpp
+++ ac1414Project1.cpp
@@ -3,11 +3,14 @@
  * ame0205
  * etp0040
  */
-
 #include <iostream>
 using namespace std;
 
 int main() {
+    int i;
+    int minVal;
+    string usr_sentence;
+    string enteredSent;
     int length;
     cin >> length; //must be odd. if even, ask again until odd.
     enum Shape {Square=1, Triangle=2, Pentagon=3, Sentence=4, Quit=5};
@@ -26,6 +29,40 @@ int main() {
             break;
         case Sentence:
             /* Sentence for animation */
+            cout << "Enter a sentence:" << endl; // Prompt user input
+            cin.ignore(); // allow for input to be read
+            getline(cin,usr_sentence);
+            enteredSent = usr_sentence;
+            system ("clear"); // clearing screen
+            // main loop    
+            minVal = 0;
+            for (i = 0; i < usr_sentence.size(); i++) // for every element of users input
+            {
+                if (isdigit(usr_sentence[i])) 
+                {
+                    if (usr_sentence[i] > minVal)
+                    {
+                    minVal = usr_sentence.at(i);
+                    }
+                    usr_sentence.erase(i, 1);
+                    sleep(1);
+                    i --;
+                    cout << usr_sentence << endl;
+
+
+                }
+           
+                
+                     // decrease i o that it moves to the next element in string
+                    if (!(isdigit((minVal)))) // if if its not a digit or char at i index cout nothign
+
+                    {
+                        continue;
+                    }
+
+                }
+            
+                cout << "Entered sentence: " << enteredSent << endl; // last line on output
             break;
         case Quit:
             /* Ask user to continue program. If not, end program */
